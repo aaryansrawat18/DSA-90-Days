96e75625cb408e3d5b571b429c78423e8704dc24
Aaryan Singh Rawat <114087827+aaryansrawat18@users.noreply.github.com>
2025-06-19 23:35:57 +0530

    Time: 93 ms (15.72%), Space: 56.1 MB (56.07%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        
+        List<Integer> ans = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int ele : nums) {
+            pq.add(ele);
+            if (pq.size() > nums.length) {
+                ans.add(pq.remove());
+            }
+        }
+
+        while (pq.size()>0) {
+            ans.add(pq.remove());
+        }
+
+        // Convert List<Integer> to int[]
+        int[] array = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            array[i] = ans.get(i);
+        }
+
+        return array;
     }
 }
