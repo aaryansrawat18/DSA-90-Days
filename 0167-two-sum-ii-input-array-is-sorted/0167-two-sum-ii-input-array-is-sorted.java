e9500d029f9c50360b699f9334cd1816eb192e62
Aaryan Singh Rawat <114087827+aaryansrawat18@users.noreply.github.com>
2025-09-17 20:40:00 +0530

    Time: 2 ms (92.6%), Space: 47.4 MB (27.4%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,6 +1,23 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-HashMap<Integer, Integer> map = new HashMap<>(); for (int i = 0; i < arr.length; i++) { int main = target - arr[i]; if (map.containsKey(main)) { return new int[]{map.get(main)+1, i+1}; } map.put(arr[i], i); } return new int[]{};
+        int left=0;
+        int right= arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            int sum =arr[left]+arr[right];
+            if(sum==target){
+                return new int[]{left+1,right+1};
+            }
+
+          else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+
+
+        }
         
+        return new int[]{};
     }
 }
