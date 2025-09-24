59157f05bdd91588d0a1879827add0dc137d8825
Aaryan Singh Rawat <114087827+aaryansrawat18@users.noreply.github.com>
2025-09-24 21:07:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (61.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,6 @@
 class Solution {
-    public double myPow(double a, int b) {
-        long exp = b; // convert int to long to avoid overflow
-
-        if (exp < 0) {
-            a = 1 / a;
-            exp = -exp;
-        }
-
-        if (exp == 0) return 1;
-
-        double half = myPow(a, (int)(exp / 2)); // recursive call
-
-        if (exp % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * a;
-        }
+    public double myPow(double x, int n) {
+       double ans= Math.pow(x,n);
+       return ans; 
     }
 }
