3cfedc4d620dd9af3a86f7ff3ab693aea35bb9b7
Aaryan Singh Rawat <114087827+aaryansrawat18@users.noreply.github.com>
2025-10-06 13:40:00 +0530

    Time: 14 ms (29.87%), Space: 45.3 MB (18.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,21 +2,13 @@ class Solution {
     
     public boolean isPalindrome(String s) {
         
-       StringBuilder sb = new StringBuilder();
+           s = s.toLowerCase();
+          String clean= s.replaceAll("[^a-z0-9]", "");
+            String reversed = new StringBuilder(clean).reverse().toString();
 
-        // 1. Build a cleaned string (lowercase + alphanumeric only)
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
-                sb.append(Character.toLowerCase(ch));
-            }
-        }
-
-        // 2. Get reversed string
-        String cleaned = sb.toString();
-        String reversed = sb.reverse().toString();
-
-        // 3. Compare cleaned string with reversed string
-        return cleaned.equals(reversed);
+      return clean.equals(reversed);
     }
 }
+
+
+   
