5e3675a94baa520af045226f5f15fa10e3f49b05
Aaryan Singh Rawat <114087827+aaryansrawat18@users.noreply.github.com>
2025-10-31 20:33:07 +0530

    Time: 3 ms (90.37%), Space: 46.7 MB (6.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,44 +1,11 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> smap= new HashMap<>();
-        for(int i=0;i<s.length();i++)
-        {
-            char key=s.charAt(i);
-            if(smap.containsKey(key))
-            {
-                int freq=smap.get(key);
-                smap.put(key,freq+1);
+          char[] sArray = s.toCharArray();
+                  char[] tArray = t.toCharArray();
 
-            }
-            else
-            {
-                smap.put(key,1);
-            }
-        }
-//1st end
- HashMap<Character, Integer> tmap= new HashMap<>();
-        for(int i=0;i<t.length();i++)
-        {
-            char key=t.charAt(i);
-            if(tmap.containsKey(key))
-            {
-                int freq=tmap.get(key);
-                tmap.put(key,freq+1);
-
-            }
-            else
-            {
-                tmap.put(key,1);
-            }
-        }
-
-        if(smap.equals(tmap)) {return true;}
-        else{
-
- return false;
-        }
-
-        //return
+        Arrays.sort( sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
         
     }
 }
